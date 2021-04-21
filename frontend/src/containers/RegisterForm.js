132838641b91dc9626bb56630024c267c0808a76
Karl Behrens <karlbehrensg@gmail.com>
2021-04-21 16:10:56 -0400

    Register page

---
+++ frontend/src/containers/RegisterForm.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import Radio from '../components/Radio'
+
+const Container = styled.div`
+  display: block;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  max-width: 350px;
+  height: 100vh;
+  max-height: 550px;
+`
+
+const HeaderLogin = styled.div`
+  width: 100%;
+  height: 20%;
+  padding: 10%;
+  margin-top: 10%;
+
+  p {
+    color: rgba(45, 55, 72, 1);
+    font-size: 16px;
+  }
+
+  h1 {
+    color: rgba(26, 32, 44, 1);
+    font-size: 30px;
+  }
+`
+
+const Form = styled.div`
+  width: 100%;
+  height: 70%;
+  padding: 0px 10% 10% 10%;
+
+  p {
+    color: rgba(74, 85, 104, 1);
+    font-size: 16px;
+  }
+
+  input {
+    border: 1px solid rgba(232, 232, 232, 1);
+    box-sizing: border-box;
+    border-radius: 5px;
+    height: 40px;
+    width: 100%;
+    background-color: rgba(247, 248, 252, 1);
+    margin: 10px 0px;
+    padding: 3%;
+  }
+
+  .option, .register{
+    color: rgba(159, 162, 180, 1);
+    font-size: 12px;
+  }
+
+  .register {
+    display: flex;
+    justify-content: center;
+    margin-top: 5px;
+  }
+`
+
+const Button = styled.button`
+  background: rgba(4, 195, 92, 1);
+  border-radius: 5px;
+  width: 100%;
+  height: 40px;
+  color: white;
+  font-weight: bold;
+  margin-top: 10px;
+  
+  :hover {
+    background: rgba(41, 204, 151, 1);
+    transition: 0.5s;
+  }
+`
+
+const RegisterForm = () => {
+  return (
+    <Container>
+      <HeaderLogin>
+        <h1>Registrate</h1>
+      </HeaderLogin>
+      <Form>
+        <p>Usuario</p>
+        <input type='text' placeholder='Correo electronico' />
+        <p>Contraseña</p>
+        <input type='password' placeholder='**********' />
+        <p>Confirma contraseña</p>
+        <input type='password' placeholder='**********' />
+        <Button>Registrate</Button>
+        <Link style={{ textDecoration: 'none' }}><p className='register'>Ya tienes una cuenta? Inicia sesion</p></Link>
+      </Form>
+    </Container>
+  )
+}
+
+export default RegisterForm
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
+import RegisterForm from '../containers/RegisterForm'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`
 
 const Register = () => {
   return (
-    <h1>Register</h1>
+    <Container>
+      <RegisterForm />
+    </Container>
   )
 }
 
