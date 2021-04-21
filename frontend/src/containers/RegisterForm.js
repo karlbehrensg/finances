6594da2c312881df0b22c6c77be60a0d015e0e6b
Karl Behrens <karlbehrensg@gmail.com>
2021-04-21 16:11:20 -0400

    Delete innecesary import

--- frontend/src/containers/RegisterForm.js
+++ frontend/src/containers/RegisterForm.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import Radio from '../components/Radio'
 
 const Container = styled.div`
   display: block;
