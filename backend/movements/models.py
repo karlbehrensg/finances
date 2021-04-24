43048cd72c6d20bfefe5b79033900959d44a7f27
Karl Behrens <karlbehrensg@gmail.com>
2021-04-24 17:40:39 -0400

    Change expired value to object DateField

--- backend/movements/models.py
+++ backend/movements/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Movement(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
-    expired = models.DateTimeField()
+    expired = models.DateField()
     agent = models.CharField(max_length=30, blank=True, default='')
     income = models.BooleanField(default=False)
     amount = models.IntegerField(default=0)
