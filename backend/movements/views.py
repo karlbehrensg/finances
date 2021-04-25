e2fe1db9351016b07682aa03c85876524dcda11f
Karl Behrens <karlbehrensg@gmail.com>
2021-04-25 19:19:44 -0400

    Add movement in backend API

--- backend/movements/views.py
+++ backend/movements/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from .models import Movement
 from .serializers import MovementSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def movements_list(request):
     if request.method == 'GET':
         user = request.user
@@ -12,3 +12,10 @@ def movements_list(request):
         serializer = MovementSerializer(movement, many=True)
         response = {'movements': serializer.data}
         return Response(response)
+
+    if request.method == 'POST':
+        serializer = MovementSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
