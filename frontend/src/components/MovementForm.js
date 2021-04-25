f76def0583eb8fd0c9074fb035b90af292764a9a
Karl Behrens <karlbehrensg@gmail.com>
2021-04-25 15:58:40 -0400

    Movement form container like modal

--- frontend/src/components/MovementForm.js
+++ frontend/src/components/MovementForm.js
@@ -1,20 +1,35 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import styled from 'styled-components'
 
 const Container = styled.div`
-  display: flex;
-  width: 100vw;
-  height: 100vh;
-  position: absolute;
-  justify-content: center;
-  align-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 50px;
+  z-index: 1000;
+`
+
+const FormContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  z-index: 1000;
 `
 
 const MovementForm = () => {
-  return (
+  return ReactDOM.createPortal(
     <Container>
-      <h1>Formulario de movimiento</h1>
-    </Container>
+      <FormContainer>
+        <h1>Formulario de movimiento</h1>
+      </FormContainer>
+    </Container>,
+    document.getElementById('portal')
   )
 }
 
