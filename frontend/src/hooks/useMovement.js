a7a971bc5ec3952e4e9cc8fdcfcba70417dc5bfc
Karl Behrens <karlbehrensg@gmail.com>
2021-04-27 19:16:40 -0400

    Update frontend when movement it's edited

--- frontend/src/hooks/useMovement.js
+++ frontend/src/hooks/useMovement.js
@@ -1,14 +1,18 @@
 import { useContext, useCallback } from 'react'
 import Context from '../context/UserContext'
 import createMovementService from '../services/createMovement'
+import getMovements from '../services/getMovements'
 
 const useMovement = () => {
-  const { jwt } = useContext(Context)
+  const { jwt, setMovements } = useContext(Context)
 
   const createMovement = useCallback(({ id, expired, agent, income, amount, category }) => {
     const method = (id) ? 'PUT' : 'POST'
 
     createMovementService({ jwt, id, expired, agent, income, amount, category, method })
+      .then(() => {
+        getMovements({ jwt }).then(setMovements)
+      })
       .catch(err => {
         console.error(err)
       })
