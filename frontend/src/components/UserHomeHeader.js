9ed08e37475b01229096a736508b66ccec9bc952
Karl Behrens <karlbehrensg@gmail.com>
2021-04-29 23:18:22 -0400

    Put current date in option

--- frontend/src/components/UserHomeHeader.js
+++ frontend/src/components/UserHomeHeader.js
@@ -40,18 +40,49 @@ const Form = styled.form`
   width: 164px;
 `
 
+const today = new Date()
+const currentMonth = today.getMonth()
+const currentYear = today.getFullYear()
+
+const years = Array(currentYear - 2020 + 2).fill().map((_, idx) => 2020 + idx)
+
+const months = [
+  { month: 'Enero', value: 0 },
+  { month: 'Febrero', value: 1 },
+  { month: 'Marzo', value: 2 },
+  { month: 'Abril', value: 3 },
+  { month: 'Mayo', value: 4 },
+  { month: 'Junio', value: 5 },
+  { month: 'Julio', value: 6 },
+  { month: 'Agosto', value: 7 },
+  { month: 'Septiembre', value: 8 },
+  { month: 'Octubre', value: 9 },
+  { month: 'Noviembre', value: 10 },
+  { month: 'Diciembre', value: 11 }
+]
+
+const yearOptions = years.map((year) =>
+  <option value={year.value} key={year}>
+    {year}
+  </option>
+)
+
+const monthOptions = months.map((month, index) =>
+  <option value={month.value} key={index}>
+    {month.month}
+  </option>
+)
+
 const UserHomeHeader = () => {
   return (
     <>
       <Title>Resumen</Title>
       <Form>
-        <Select name='month' id='month'>
-          <option value='march'>Marzo</option>
-          <option value='april'>Abril</option>
+        <Select name='month' id='month' defaultValue={currentMonth}>
+          {monthOptions}
         </Select>
-        <Select name='year' id='year'>
-          <option value='2020'>2020</option>
-          <option value='2021'>2021</option>
+        <Select name='year' id='year' defaultValue={currentYear}>
+          {yearOptions}
         </Select>
         <Button type='button'>Ir</Button>
       </Form>
